85c3afc3f58fa90a24efc92dd1e9e91117dc012c
Marco Aurélio Prado <marco.pdsv@gmail.com>
2018-09-06 13:59:55 -0300

    accounts in plural

--- routes/gps_data.js
+++ routes/gps_data.js
@@ -1,6 +1,6 @@
 const router = new (require('restify-router')).Router();
 
-router.get('/api/account/:accountId/gps-data', function (req, res, next) {
+router.get('/api/accounts/:accountId/gps-data', function (req, res, next) {
 	res.json({
 		message: 'Lista com todas gps-data',
 		query: req.query
