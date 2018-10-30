b3fd0709c177254b1aaab382c0bba78377a06ae0
Marco Aurélio Prado <marco.pdsv@gmail.com>
2018-10-30 17:47:31 -0300

    Decreasing the length of the lines

--- resources/gpsData/model.js
+++ resources/gpsData/model.js
@@ -3,35 +3,44 @@ const timestamps = require('mongoose-timestamp');
 
 const GpsDataSchema = new mongoose.Schema({
     data: {
-        encryptedGpsData: {
+        // Latitude e longitude encriptada
+        encryptedGpsData: { 
             type: String,
             required: true,
         },
-        creationUnixTimestamp: {
+        // Unix timestamp de quando essa amostra
+        // de sinal GPS foi obtida
+        creationUnixTimestamp: {   
             type: Number,
             required: true,
         }
     },
+    // Endereço de quem enviou tal dado
     from: {
         type: String,
         require: true
     },
+    // Reconver id
     v: {
         type: Number,
         required: true
     },
+    // Parâmetro r da assinatura ECDSA
     r: {
         type: Buffer,
         required: true
     },
+    // Parâmetro s da assinatura ECDSA
     s: {
         type: Buffer,
         required: true
     },
+    // Endereço ethreum da conta que envou tal sinal
     accountId: {
         type: String,
         required: true,
     },
+    // Endereço Ethereum do contrato
     contractId: {
         type: String,
         required: true,
--- resources/gpsData/routes.js
+++ resources/gpsData/routes.js
@@ -1,15 +1,19 @@
 const router = new (require('restify-router')).Router();
 const GpsDataSchema = require('./model');
 
+// Endpoint para obtenção dos dados do GPS
 router.get('/', function (req, res, next) {
-	let limit = parseInt(req.query.limit, 10) || 10; // default limit to 10 docs
-	let skip  = parseInt(req.query.skip, 10) || 0; // default skip to 0 docs
+	// default limit to 10 docs
+	let limit = parseInt(req.query.limit, 10) || 10; 
+	// default skip to 0 docs
+	let skip  = parseInt(req.query.skip, 10) || 0; 
 	let query = req.params || {};
 
 	// remove skip and limit from data to avoid false querying
 	delete query.skip
 	delete query.limit
 
+	// Obtendo um determinado dado do GPS
 	GpsDataSchema
 		.find(query)
 		.skip(skip)
@@ -23,11 +27,19 @@ router.get('/', function (req, res, next) {
 		})
 });
 
+// Enviando uma amostra do sinal GPS para o banco de dados
 router.post('/', function (req, res, next) {
 	// console.log(req.params);
-	let data = Object.assign({}, { accountId: req.params.accountId, contractId: req.params.contractId }, req.body) || {}
+	// Adicionando accountId e contractId ao dado que será 
+	// armazenado no banco de dados
+	let data = Object.assign({}, 
+		{ 	
+			accountId: req.params.accountId, 
+			contractId: req.params.contractId 
+		}, req.body) || {}
 	// console.log(data);
 
+	// Enviando para o banco de dados o dado
 	GpsDataSchema
 		.create(data)
 		.then(gpsData => {
